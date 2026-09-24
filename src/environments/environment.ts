20e01232c2566856d6a3deb9a2a94f2338c851a5
brstockle <br.stockle@duocuc.cl>
2026-09-24 10:47:35 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  
+  // 👉 Descomenta la URL que quieras usar y guarda el archivo. Angular cambiará todo automáticamente.
+
+  // OPCIÓN 1: Entorno Local
+  apiUrl: 'http://localhost:8080/api/bff'
+
+  // OPCIÓN 2: Entorno AWS (Producción)
+  // apiUrl: 'https://3lgyldt561.execute-api.us-east-1.amazonaws.com/api/bff'
+};
